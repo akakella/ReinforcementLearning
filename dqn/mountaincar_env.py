2d2dd05edf9f2c717ed7dba2d4bd4c61d6f798c9
Abishek <abishek.akella@gmail.com>
2017-06-18 17:16:40 -0400

    Adding mountaincar-v0 solution

--- dqn/mountaincar_env.py
+++ dqn/mountaincar_env.py
@@ -3,6 +3,7 @@ from gym import wrappers
 from agents.dqn_agent import dqn_agent
 import math
 import numpy as np
+import pandas as pd
 
 """
 Trains the MountainCar-v0 environment from OpenAI's gym for a predefined number of episodes
@@ -10,19 +11,26 @@ Trains the MountainCar-v0 environment from OpenAI's gym for a predefined number 
 """
 
 def main(episodes):
-	env = gym.make('LunarLander-v2')
-	env = wrappers.Monitor(env, '../tmp/cartpole-experiment', force=True)
+	# Init replay memory
+	env = gym.make('MountainCar-v0')
+	replay_agent = dqn_agent(env, layers=[128,256,128], decay_rate=0.02, batch_size=64, target_update_frequency=200)
+	replay_agent.init_replay_memory()
 
-	agent = dqn_agent(env, layers=[2,128], decay_rate=0.02)
+	train_env = gym.make('MountainCar-v0')
+	train_env = wrappers.Monitor(train_env, '../tmp/mountaincar-experiment', force=True)
+	agent = dqn_agent(train_env, layers=[128,256,128], decay_rate=0.02, batch_size=64, target_update_frequency=200)
+	agent.replay_memory = replay_agent.replay_memory
 	rewards = agent.train(episodes)
 
 
 	print('Maximum reward obtained: ' + repr(max(rewards)))
+	env.close()
+	train_env.close()
+
 	reward_frame = pd.DataFrame(rewards)
 	rolling_maxes = reward_frame.rolling(window=100, center=False).mean()
 	print('Best reward obtained over 100 episodes: ' + repr(np.array(np.max(rolling_maxes))[0]))
-	env.close()
 
 if __name__ == "__main__":
-	episodes = 2000
+	episodes = 1000
 	main(episodes)
